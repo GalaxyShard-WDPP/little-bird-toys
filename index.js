4a6666524a94fe01ae5e68fc4517fbcb85fb0049
GalaxyShard <swift.galaxyshard@gmail.com>
2023-05-19 15:15:22 +0000

    final draft

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
 let imageCounts = {
-    block: 5,
-    boat:2,
-    car:4,
-    plane:3,
-    train:7,
+    placeholder: 1,
+    block: 3,
+    boat: 1,
+    car: 4,
+    plane: 3,
+    train: 7,
 };
+let categoryCount = 6;
 
 let toyImage = document.getElementById("toy-image");
 let toySelector = document.getElementById("toy-selector");
+let carouselElement = document.getElementById("carousel");
+let carousel = new bootstrap.Carousel(carouselElement)
 
-toySelector.addEventListener("change", () => {
-    let selection = toySelector.value;
-    let index = Math.floor(Math.random() * imageCounts[selection] + 1);
+let clearId = null;
+let imageIndex = -1;
+function genImage(type) {
+    toyImage.src = `img/${type}${genIndex(type)}.png`;
+}
+function genIndex(type) {
+    imageIndex = (imageIndex + 1) % imageCounts[type];
+    return imageIndex;
+}
+genImage("block");
+
+let toyButtons = toySelector.querySelectorAll("button")
+for (let button of toyButtons) {
+    button.addEventListener("click", () => {
+        let selectedOption = button.dataset.type;
+        let selectedIndex = button.dataset.index;
+        
+        imageIndex = -1;
+        
+        genImage(selectedOption);
+        // if (clearId !== null) {
+        //     clearInvterval(clearId);
+        // }
+        // clearId = setInterval(() => {
+        //     genImage(selectedOption);
+        // }, 3000);
+        // if (selectedOption !== "placeholder") {
+        //     if (clearId !== null) {
+        //         clearInterval(clearId);
+        //     }
+        //     clearId = setInterval(() => {
+        //         genImage(selectedOption)
+        //     }, 3000);
+        // } else {
+        //     clearInterval(clearId);
+        //     clearId = null;
+        // }
     
-    toyImage.src = `img/${selection}${index}.jpg`;
-});
+        carousel.to(selectedIndex);
+    });
+}
